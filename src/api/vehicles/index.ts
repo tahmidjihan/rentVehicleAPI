0e2e3563157e6eb6e5b2332744aff95f9114f849
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-12-05 21:20:47 +0600

    bookings and vehicles

--- src/api/vehicles/index.ts
+++ src/api/vehicles/index.ts
@@ -6,4 +6,12 @@ router.get('/', auth, (req, res) => {
   console.log(req.user);
   res.send('Welcome to the Vehicles API!');
 });
+router.post('/', auth, (req, res) => {
+  const user = req.user;
+  if (user?.role !== 'admin') {
+    res.status(401).send('Access denied');
+    return;
+  }
+  res.send('Welcome to the Vehicles API!');
+});
 export default router;
--- src/initDB.ts
+++ src/initDB.ts
@@ -11,6 +11,23 @@ function initDB() {
         phone VARCHAR(15) NOT NULL,
         role VARCHAR(10) NOT NULL
       );
+      CREATE TABLE IF NOT EXISTS vehicles (
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(255) NOT NULL,
+        type VARCHAR(255) NOT NULL,
+        registration_number VARCHAR(255) NOT NULL,
+        daily_rent_price INT NOT NULL CHECK(daily_rent_price > 0),
+        availability_status VARCHAR(255) NOT NULL
+      );
+      CREATE TABLE IF NOT EXISTS bookings (
+        id SERIAL PRIMARY KEY,
+        customer_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+        vehicle_id INT NOT NULL REFERENCES vehicles(id) ON DELETE CASCADE,
+        rent_start_date DATE NOT NULL,
+        rent_end_date DATE NOT NULL CHECK(rent_end_date > rent_start_date),
+        total_price INT NOT NULL CHECK(total_price > 0),
+        status VARCHAR(255) NOT NULL
+      );
     `
   );
 }
