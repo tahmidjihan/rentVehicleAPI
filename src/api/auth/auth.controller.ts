a9b0941ecc438df36b60f48b41566b6273709d79
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-12-07 16:23:31 +0600

    modular auth

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,91 +1,21 @@
-import { dbPool } from '../../dbPool';
-import {
-  type Credentials,
-  type User,
-  type UserResponse,
-} from '../../types/User.d';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import config from '../../config';
+// import { signin, signup } from './auth.service';
+import type { Credentials } from '../../types/User';
+import express from 'express';
+import service from './auth.service';
 
-// hashing
-
-async function hashPassword(password: string) {
-  const saltRounds = 11;
-  let hashedPassword: string = '';
-  let pending = true;
-  //   function setHashedPassword(hashed: string) {
-  //     hashedPassword = hashed;
-  //   }
-  bcrypt.genSalt(saltRounds, function (err, salt) {
-    bcrypt.hash(password, salt, async function (err, hash) {
-      if (hash) {
-        hashedPassword = hash;
-        pending = false;
-      }
-    });
-  });
-  while (pending) {
-    await new Promise((resolve) => setTimeout(resolve, 100));
+async function signup(req: express.Request, res: express.Response) {
+  const user = await req.body.user;
+  res.send(service.signup(user));
+}
+async function signin(req: express.Request, res: express.Response) {
+  const credentials: Credentials = await req.body.credentials;
+  // console.log(req.body);
+  // res.send(signin(credentials));
+  const result = await service.signin(credentials);
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(401).send('Access denied');
   }
-  return hashedPassword;
 }
-async function comparePassword(hashedPassword: string, password: string) {
-  let pending = true;
-  let resultToReturn = false;
-  bcrypt.compare(password, hashedPassword, function (err, result) {
-    if (err) {
-      console.log(err);
-    }
-    resultToReturn = result;
-    pending = false;
-    // console.log(result);
-    return result;
-  });
-  while (pending) {
-    await new Promise((resolve) => setTimeout(resolve, 100));
-  }
-  return resultToReturn;
-}
-
-//auth
-export async function signup(user: User) {
-  const password = user.password;
-  const hashedPassword = await hashPassword(password);
-  const data = await dbPool.query(
-    'INSERT INTO users (name, email, password, phone, role) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-    [user.name, user.email, hashedPassword, user.phone, user.role]
-  );
-  //   console.log(data);
-  return data;
-}
-function signJWT(user: UserResponse) {
-  return jwt.sign(user, config.JWT_SECRET);
-}
-export async function signin({ email, password }: Credentials) {
-  const data = await dbPool.query('SELECT * FROM users WHERE email = $1', [
-    email,
-  ]);
-  const user = data.rows[0];
-  if (user) {
-    const hashedPassword = user.password;
-    const result = await comparePassword(hashedPassword, password);
-    if (result) {
-      const userToSend: UserResponse = {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        phone: user.phone,
-        role: user.role,
-      };
-      const token = signJWT(userToSend);
-      // userToSend.token = token;
-      const result = { user: { ...userToSend }, token: token };
-      return result;
-    } else {
-      return null;
-    }
-    // console.log(result);
-  }
-  // return null;
-}
+export default { signup, signin };
--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,25 +1,10 @@
 import express from 'express';
-import { signin, signup } from './auth.controller';
-import type { Credentials } from '../../types/User';
-
+import controller from './auth.controller';
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Welcome to the Auth API!');
 });
-router.post('/signup', async (req: express.Request, res: express.Response) => {
-  const user = await req.body.user;
-  res.send(signup(user));
-});
-router.post('/signin', async (req: express.Request, res: express.Response) => {
-  const credentials: Credentials = await req.body.credentials;
-  // console.log(req.body);
-  // res.send(signin(credentials));
-  const result = await signin(credentials);
-  if (result) {
-    res.send(result);
-  } else {
-    res.status(401).send('Access denied');
-  }
-});
+router.post('/signup', controller.signup);
+router.post('/signin', controller.signin);
 export default router;
